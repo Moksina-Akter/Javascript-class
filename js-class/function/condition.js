a2629fe9465e86db7e737c597ac5e005b9708e2c
Moksina-Akter <msdiya01865@gmail.com>
2025-01-27 12:06:13 +0600

    index added

---
+++ js-class/function/condition.js
@@ -0,0 +1,136 @@
+/*
+let days;
+days= +prompt();
+if(days===1){
+   document.write('Sunday');
+}
+else if(days===2){
+    document.write('Monday') ;
+}
+else if(days===3){
+    document.write('Tuesday') ;
+}
+else if(days===4){
+    document.write('Wednesday'); 
+}
+else if(days===5){
+    document.write('Thursday') ;
+}
+else if(days===6){
+    document.write('Friday') ;
+}
+else if(days===7){
+    document.write('Saturday') ;
+}
+else{
+    document.write('You entered invalid number');
+}
+*/
+
+
+
+
+// _____________switch(day)______________
+/*
+let days = +prompt();
+
+switch(days){
+
+    case 1:
+        document.write('sunday');
+        break;
+
+    case 2:
+        document.write('monday');
+      break;
+
+    case 3:
+        document.write('tuesday');
+      break;
+
+    case 4:   
+        document.write('wednesday');
+        break;
+
+    case 5:
+            document.write('thrusday');
+            break;
+    case 6:
+    document.write('friday');
+    break;  
+
+    case 7:
+        document.write('saturday')
+        break;
+
+    default:
+        document.write('invalid')
+}
+*/
+        
+
+
+// ___________________working day___________
+/*
+let days = +prompt();
+
+switch(days){
+
+    case 1:
+        
+    case 2:
+       
+    case 3:
+        
+
+    case 4:   
+        
+
+    case 5:
+            document.write('working day');
+            break;
+    case 6:      
+    case 7:
+    document.write('sleeping day');
+    break;  
+
+    default:
+        document.write('invalid')
+    
+} 
+*/
+    
+
+ // ________________vowel___________
+/*
+let alphabet = prompt();
+let alph=alphabet.toLowerCase();
+
+switch(alph){
+
+    case 'a':
+        document.write('vowel')
+        break;
+
+    case 'e':
+            document.write('vowel')
+            break; 
+            
+    case 'i':
+        document.write('vowel')
+        break;
+        case 'o':
+        document.write('vowel')
+        break;
+        case 'u':
+            document.write('vowel')
+            break; 
+        default:
+            document.write('consonant')
+}
+*/
+
+
+
+
+
---
+++ js-class/index8.js
@@ -0,0 +1,39 @@
+
+
+/*
+//-----------trim----------
+
+let myName = 'Muksina';
+let myName2 = '  Muksina ';
+
+if(myName .toLowerCase() .trim()===myName2 .toLowerCase() .trim()){
+    console.log('Yes its me');
+}
+else{
+    console.log('No i am not');
+}
+
+console.log(myName.length);
+console.log(myName2.length);
+
+*/
+
+
+//-------Array vs string similarity---------
+
+let students = ['Meem','Muksina','Hiya','Priya','Liza'    ];
+console.log(students);
+console.log(students[3]);
+students[3] = 'Diya';
+
+
+var firstName = 'Muksina    ';
+
+console.log(firstName[3]);
+firstName[3] ='m';
+console.log(firstName);
+
+
+console.log(students.length);
+console.log(firstName,length);
+
