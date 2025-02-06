bcc2c93af735cead48c1a4aaa15881baf2f265f7
Moksina-Akter <msdiya01865@gmail.com>
2025-02-06 11:50:43 +0600

    edit

---
+++ js-class/function/function5.js
@@ -0,0 +1,90 @@
+/*
+function sum(){
+
+    const num1 =100;
+    const num2 =500;
+    
+    const result =num1+ num2;
+
+    console.log(result);
+}
+sum();
+*/
+
+/*
+const num1 =100;
+
+function sum(){
+     
+    const num2 =500;
+    const result =num1+ num2;
+
+    console.log(result);
+}
+sum();
+
+/*
+const num1 =100;
+
+function sum(){
+     
+    const num1 =120;
+    const num2 =500;
+    const result =num1+ num2;
+
+    console.log(result);
+}
+sum();
+*/
+
+/*
+function sum(A,B){
+
+    const result = A+B;
+    console.log(result);
+}
+sum(100,400);
+*/
+/*
+!-----------------Return------------------------
+function sum(A){
+
+    const result = A*20;
+    return result;
+}
+console.log(sum(10));
+*/
+/*
+ ? --------shortcut----------
+function sum (A){
+    return A*20;
+
+}
+console.log(sum(10));
+*/
+
+
+
+
+
+
+
+
+function calculation(num1, num2){
+
+    const add = num1+num2;
+    const sub = num1-num2;
+    const mul = add * sub;
+    const div = mul/2;
+    const result =div;
+    return result;
+}
+
+const result= calculation(12, 3)
+console.log(result);
+
+
+
+
+
+
