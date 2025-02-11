1fac7160e953020d72df199eb6ca750db387e24a
Moksina-Akter <msdiya01865@gmail.com>
2025-02-11 10:58:34 +0600

    index add

---
+++ js-class/function/array.js
@@ -0,0 +1,27 @@
+
+/*
+function add(num){
+    let sum = 0;
+    for (const arr of num) {
+        console.log(arr);
+        sum = sum + arr;
+    }
+    return sum;
+  }
+  const numbers = [40, 20, 30, 50, 60];
+  const total = add(numbers); // ফাংশন থেকে রিটার্ন নেওয়া
+  console.log('Sum of the numbers is', total);
+
+  */
+
+
+const numbers=[10,20,30,40,50,60,70,80];
+function sumOfArray (array){
+
+    for (const element of array) {
+        
+    }
+}
+  
+const result = sumOfArray(numbers);
+console.log(result);
