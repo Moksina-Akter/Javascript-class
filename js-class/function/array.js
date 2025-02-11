b5cbd5284811d6d31aed8c4a10d287af8931c437
Moksina-Akter <msdiya01865@gmail.com>
2025-02-11 11:40:53 +0600

    index add

--- js-class/function/array.js
+++ js-class/function/array.js
@@ -14,14 +14,70 @@ function add(num){
 
   */
 
-
+/*
 const numbers=[10,20,30,40,50,60,70,80];
 function sumOfArray (array){
 
+    let sum=0;
     for (const element of array) {
         
+      sum= sum+element;
+      //console.log(sum);
+
+     //return sum;
     }
+    //console.log(sum);
+
+    return sum;
 }
-  
 const result = sumOfArray(numbers);
 console.log(result);
+*/
+
+/*
+const nums = [9,8,7,6,5,4,3,2,1];
+function addition(newArray){
+
+    let sum= 0;
+    for (const arr of newArray) {
+        sum = sum+arr;
+
+        //console.log(sum);
+        //return sum;
+    }
+    //console.log(sum);
+    return sum;
+}
+
+const result = addition(nums);
+console.log(result);
+*/
+
+
+/*
+
+let nums=[10,11,12,13,14,15,16,17,18,19,20];
+function add (array){
+
+    let sum =0;
+    for (const arr of array) {
+        sum = sum +arr;
+        
+        //console.log(sum);
+        //return sum;
+    }
+    //console.log(sum);
+    return sum;
+}
+
+const rst = add(nums);
+console.log(rst);
+
+*/
+
+
+
+
+
+
+
