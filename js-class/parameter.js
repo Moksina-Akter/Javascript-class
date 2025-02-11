c0fa9b8aa12d315f466cfdaebf2303415619e6d3
Moksina-Akter <msdiya01865@gmail.com>
2025-02-11 10:32:19 +0600

    edit

---
+++ js-class/parameter.js
@@ -0,0 +1,145 @@
+/*
+!-------------Various types of parameter for a function 
+--------------
+ ? Variable :
+function myFunction (num,num1){
+  console.log(num,num1);
+}
+myFunction(10,15)
+myFunction(20)
+*/
+
+/*
+let name1 = 'Sabrina';
+let name2 = 'Enna';
+
+function passVar (name1,name2){
+  console.log(name1,name2);
+}
+passVar(name1,name2)
+*/
+/*
+let name1 = 'Sabrina';
+let name2 = 'Enna';
+
+function passVar (){
+  console.log(name1,name2);
+}
+passVar(name1,name2)
+*/
+/*
+let name1 = 'Sabrina';
+let name2 = 'Enna';
+
+function passVar (stn1,stn2){
+  console.log(stn1,stn2);
+}
+passVar(name1,name2)
+*/
+
+
+/*
+ ? Number:
+let num1 = '12';
+let num2 = '16';
+
+function passVar (stn1,stn2){
+  console.log(stn1,stn2);
+}
+passVar(num1,num2)
+*/
+
+/*
+? String:
+
+function string(name){
+   const lng = name.length;
+
+    //console.log(lng);
+    console.log(name,lng)
+
+   //console.log(name);
+   //console.log(name.length);
+}
+string('maria');
+string('I am lucky girl');
+string('My father is my world');
+*/
+
+/*
+? Boolean:
+
+function doSomething (number,calculate){
+   if(calculate===true){
+       console.log(number+10);
+   }
+   else{
+       console.log(number-10);
+   }
+}
+doSomething(50,false);
+*/
+/*
+function doSomething (number,calculate){
+   if(calculate===true){
+    const getTaka = number+20;
+       console.log(getTaka);
+   }
+   else{
+    const giveTaka = number-20;
+       console.log(giveTaka);
+   }
+}
+doSomething(50,true);
+doSomething(100,false);
+*/
+
+/*
+! --------------condition------------
+function string (num){
+  if(num.length%2===0){
+    console.log('even');
+
+  }
+  else{
+     console.log('odd')
+  }
+}
+
+string('muksina');
+string('muksin');
+*/
+
+/*
+? shortcut:
+function string (num){
+  num.length%2===0? console.log('Even'): console.log('odd');
+
+}
+string('Diya');
+*/
+
+
+/*
+function array (name){
+  console.log(name.includes('Meem'))
+  console.log(name.indexOf(2))
+  console.log(name.push('sabiha'))
+  
+}
+array(['Maria','Sultana','Meem','Muksina'])
+
+
+
+function object (a){
+   console.log(a);
+
+}
+object( newOb ={
+      names:'maria',
+       address:'Kutubpur'
+} )
+       
+*/
+
+  
--- js-class/paramitre.js
+++
@@ -1,4 +0,0 @@
-function myFunction (num){
-  console.log(num);
-}
-myFunction(10)
